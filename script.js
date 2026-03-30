258fe0e19ccf22119871a8f64d3d2b811815a420
Cipher95 <h.alobaidan95@gmail.com>
2026-03-30 20:08:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ let currentLang = 'en'; // Default language
                 alt_network: "Online Network Battle",
                 alt_preferences: "Online Preferences",
                 alt_rankings: "Online Rankings",
-                characters_title: "KEY CHARACTERS", special_art_title: "SPECIAL ART", steam_charts_title: "STEAM CHARTS", details_title: "GAME DETAILS", detail_label_available: "Available", detail_label_players: "Players",
+                characters_title: "KEY CHARACTERS", special_art_title: "SPECIAL ART", steam_charts_title: "STEAM CHARTS", details_title: "GAME DETAILS", detail_label_release_date: "Release Date", detail_label_players: "Players",
                 detail_label_platforms: "Platforms", detail_label_rating: "Rating", detail_label_genre: "Genre", detail_label_note: "Note",
                 detail_value_players: "1 player (up to 2 players online)", detail_value_rating: "ESRB E10+", detail_value_note1: "Contents of game screenshots may differ from final product.", detail_value_note2: "Crossplay and cross-save between platforms is not supported.", detail_label_genre: "Action RPG",
                 detail_label_supported_languages: "Supported languages", detail_value_supported_languages: "Text: Japanese/English/Traditional Chinese/Simplified Chinese Voice: Japanese/English"
@@ -108,7 +108,7 @@ let currentLang = 'en'; // Default language
                 alt_network: "オンラインネットワークバトル",
                 alt_preferences: "オンライン各種設定",
                 alt_rankings: "オンラインランキング",
-                characters_title: "主な登場人物", special_art_title: "スペシャルアート", steam_charts_title: "Steamチャート", details_title: "ゲーム詳細", detail_label_available: "発売日", detail_label_players: "プレイヤー数",
+                characters_title: "主な登場人物", special_art_title: "スペシャルアート", steam_charts_title: "Steamチャート", details_title: "ゲーム詳細", detail_label_release_date: "発売日", detail_label_players: "プレイヤー数",
                 detail_label_platforms: "プラットフォーム", detail_label_rating: "CERO", detail_label_genre: "ジャンル", detail_label_note: "備考",
                 detail_value_players: "1人（オンライン最大2人）", detail_value_rating: "B（12歳以上対象）", detail_value_note1: "ゲーム画面は開発中のものであり、実際の製品とは異なる場合があります。", detail_value_note2: "プラットフォーム間のクロスプレイおよびクロスセーブはサポートされていません。", detail_label_genre: "ブラザーアクションRPG",
                 detail_label_supported_languages: "対応言語", detail_value_supported_languages: "表示言語： 日本語/英語/簡体中文/繁体中文 音声： 日本語/英語"
@@ -117,7 +117,7 @@ let currentLang = 'en'; // Default language
     };
 
     const gameDetails = [
-        { label_key: "detail_label_available", value_key: "release_date" },
+        { label_key: "detail_label_release_date", value_key: "release_date" },
         { label_key: "detail_label_players", value_key: "detail_value_players" },
         { label_key: "detail_label_platforms", value: "Nintendo Switch™ / PlayStation®5 / PlayStation®4 / Xbox Series X|S / Xbox One / PC(Steam)" },
         { label_key: "detail_label_rating", value_key: "detail_value_rating" },
